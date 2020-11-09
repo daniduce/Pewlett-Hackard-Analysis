f3a5b96d4ee1f78937a5c29af2009d27e242a68c
danidee <danielle@Danielles-MacBook-Pro.local>
2020-11-08 23:18:49 -0500

    final push for challenge

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,1 +1,49 @@
+SELECT e.emp_no, 
+		e.first_name, 
+		e.last_name,
+		ts.title,
+		ts.from_date,
+		ts.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ts
+ON (e.emp_no = ts.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no DESC
+
+SELECT emp_no, first_name, last_name, title
+FROM retirement_titles
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+SELECT COUNT(de.dept_no), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+LEFT JOIN dept_emp as de
+ON ut.emp_no = de.emp_no
+GROUP BY ut.title;
+
+SELECT DISTINCT ON (emp_no) e.emp_no, 
+		e.first_name, 
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		ts.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ts
+ON (e.emp_no = ts.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no, to_date DESC;
 
